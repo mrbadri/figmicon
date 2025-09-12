68900f80084f6b2a0efac2d58389b4478181b16d
mrbadri <badri0000badri@gmail.com>
2025-09-12 04:07:31 -0700

    feat: enhance theme configuration and add logo component; update TypeScript settings and include favicon assets for improved branding

--- apps/docs/theme.config.jsx
+++ apps/docs/theme.config.jsx
@@ -1,22 +1,51 @@
+// theme.config.tsx
+import { useTheme } from "next-themes";
+import { Logo } from "./components/logo";
+
 export default {
-  logo: <span>Iconsync Docs</span>,
-  project: {
-    link: 'https://github.com/mrbadri/iconsync',
+  logo: () => {
+    const { resolvedTheme } = useTheme();
+
+    return (
+      <span className="flex items-center gap-2">
+        {resolvedTheme === "dark" ? <Logo /> : <Logo />}
+      </span>
+    );
   },
-  docsRepositoryBase: 'https://github.com/mrbadri/iconsync/tree/main/apps/docs-nextra',
+  project: {
+    link: "https://github.com/mrbadri/iconsync",
+  },
+  docsRepositoryBase:
+    "https://github.com/mrbadri/iconsync/tree/main/apps/docs-nextra",
   footer: {
     text: `© ${new Date().getFullYear()} IconSync. All rights reserved.`,
   },
   useNextSeoProps() {
     return {
-      titleTemplate: '%s – IconSync'
-    }
+      titleTemplate: "%s – IconSync",
+    };
   },
   head: (
     <>
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta property="og:title" content="IconSync" />
       <meta property="og:description" content="IconSync documentation" />
+
+      <link rel="icon" href="/favicon.ico" sizes="any" />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon-16x16.png"
+      />
+      <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+      <meta name="theme-color" content="#121212" />
     </>
   ),
-}
+};
